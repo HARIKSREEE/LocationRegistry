81898757ac822f018403bcf87b9e195459a61805
HARIKSREEE <sivan.hari.sr@gmail.com>
2019-01-20 01:23:22 +0530

    Added welcome component

--- src/app/Login/login.component.ts
+++ src/app/Login/login.component.ts
@@ -1,14 +1,36 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { UserDetails } from '../userDetails';
 
 @Component({
-    selector:'app-login',
-    templateUrl:'./login.component.html',
-    styleUrls:['./login.component.css']
+    // selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 
-export class LoginComponent
-{
+export class LoginComponent {
 
-    title:string= 'Login';
+    title: string = 'Login';
+    userDetails: UserDetails = {
+        email: 'hari@test.com',
+        password: '12345'
+    }
+
+    userEmail: string;
+    userPassword: string;
+    isValid: boolean = false;
+    message: string;
+
+    submitDetails(): void {
+
+        if (this.userEmail == this.userDetails.email && this.userPassword == this.userDetails.password) {
+            this.isValid = true;
+            this.message = 'Login Success';
+        }
+        else {
+            this.message = 'Login Failed';
+        }
+
+    }
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule}from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {WelcomeComponenet} from './Welcome/welcome.component';
 import {LoginComponent} from './Login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponenet,
     LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
